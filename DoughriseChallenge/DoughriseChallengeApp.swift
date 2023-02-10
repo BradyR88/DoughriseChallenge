1f8eea183103215d852f5931192a8071c3e17abf
Brady Robshaw <heybrady88@gmail.com>
2023-02-10 16:22:40 -0500

    Initial Commit

---
+++ DoughriseChallenge/DoughriseChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughriseChallengeApp.swift
+//  DoughriseChallenge
+//
+//  Created by Brady Robshaw on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DoughriseChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
