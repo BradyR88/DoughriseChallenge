507020caffa98786f32845f2d5ee342d77239172
Brady Robshaw <heybrady88@gmail.com>
2023-02-13 13:00:05 -0500

    - adds a new BudgetWidgetView to WidgetGirdView that is tied to a slider to show that the widgets can dynamically update

--- DoughriseChallenge/Views/WidgetGirdView.swift
+++ DoughriseChallenge/Views/WidgetGirdView.swift
@@ -14,14 +14,23 @@ struct WidgetGirdView: View {
         GridItem(.flexible())
     ]
     
+    @State private var spent: Double = 50
+    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
                 ForEach(WidgetModel.exampleArray) { model in
                     BudgetWidgetView(model: model)
                 }
+                BudgetWidgetView(model: WidgetModel(name: "Groceries", color: .mint, bugdget: 100, spent: Int(spent)))
                 AddCategoryWidgetView()
             }
+            
+            
+            Slider(value: $spent, in: 0...100)
+                .padding(.top, 50)
+            Text("This slider is not part of the official UI it's just here to help illustrate that the widgets can dynamically update.")
+                .font(.caption)
         }
     }
 }
